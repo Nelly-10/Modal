92fc58cace4e6a30907ac93340ed5df1d15e6816
NELLY-10 <nelson__nonso@yahoo.com>
2023-03-29 20:39:43 +0100

    Second push

--- script.js
+++ script.js
@@ -23,3 +23,10 @@ for (let i = 0; i < btnOpenModal.length; i++) {
 btnCloseModal.addEventListener('click', closeModal);
 
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e) {
+      console.log(e.key);
+      if (e.key === 'Escape' && !overlay.classList.contains('hidden')) 
+            closeModal();
+      
+}) 
